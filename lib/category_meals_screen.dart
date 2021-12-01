b19c7666f58c85adf981298699b6418793d5b0c9
mostafaadawy <mostafa_adawy@ymail.com>
2021-12-01 23:20:30 +0200

    175-176-custom drawer called by hamb button and adding links to it

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -10,12 +10,12 @@ class CategoryMealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final routeArgs =
-        ModalRoute.of(context)!.settings.arguments as Map<String, String>;
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
     return Scaffold(
       appBar: AppBar(
-        title: Text(categoryTitle!),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text(
